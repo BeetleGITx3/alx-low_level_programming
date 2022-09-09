7061d194f1c2c5ee75b07bc512a7a1768ba714cf
BeetleGITx3 <jstewdier@gmail.com>
2022-09-08 23:19:26 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+int main
+#include <stdio.h>
+/**
+ *  * main - main block
+ *   * Return: Return 0
+ *    
+ *   */
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
